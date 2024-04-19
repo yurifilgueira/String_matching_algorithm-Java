008ec33295237212256827667029d1b0dea322a6
Yuri FTM <yurimedeiros141@gmail.com>
2024-04-19 04:12:07 -0300

    Maven - Implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,19 @@
 import util.DatasetReader;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         final String PATH = "resources\\train.csv";
 
         long startTime = System.currentTimeMillis();
-
-        AtomicInteger count = new AtomicInteger();
-        Map<String, Integer> matches = new HashMap<>();
 
         List<String> items = new ArrayList<>(List.of("logitech", "keyboard", "mouse", "hyperx", "razer", "lenovo", "acer", "lg", "samsung", "laptop"));
 
         DatasetReader.readDatasetAndProcess(PATH, items);
 
         System.out.println("Total read and print time: " + (double) (System.currentTimeMillis() - startTime) / 60000);
-        // System.out.println("Count: " + count);
     }
 }
