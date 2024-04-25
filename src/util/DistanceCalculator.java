44935c15821e43fb5134e0e2b3b6940095c3f9a4
Yuri FTM <yurimedeiros141@gmail.com>
2024-04-25 17:29:22 -0300

    Fix: Matches compute

--- src/util/DistanceCalculator.java
+++ src/util/DistanceCalculator.java
@@ -1,8 +1,6 @@
-
 package util;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -11,20 +9,21 @@ public class DistanceCalculator implements Runnable{
     private List<String> lines;
     private Integer start;
     private Integer end;
+    private Map<String, Integer> matches;
 
     public DistanceCalculator() {
     }
 
-    public DistanceCalculator(List<String> lines, Integer start, Integer end) {
+    public DistanceCalculator(List<String> lines, Integer start, Integer end, Map<String, Integer> matches) {
         this.lines = lines;
         this.start = start;
         this.end = end;
+        this.matches = matches;
     }
 
     public void calculateDistance() {
 
         List<String> items = new ArrayList<>(List.of("logitech", "keyboard", "mouse", "hyperx", "razer", "lenovo", "acer", "lg", "samsung", "laptop", "headset", "jbl"));
-        Map<String, Integer> matches = new HashMap<>();
 
         System.out.println(Thread.currentThread().getName() + " -> started.");
 
@@ -37,7 +36,7 @@ public class DistanceCalculator implements Runnable{
 
                 for (String word : words) {
                     if (LevenshteinDistance.calculateDistance(word, item) == 0){
-                        matches.put(word, matches.getOrDefault(word, 0) + 1);
+                        compute(word);
                         break;
                     }
                 }
@@ -47,6 +46,10 @@ public class DistanceCalculator implements Runnable{
         System.out.println(Thread.currentThread().getName() + " -> finished.");
         ResultSaver.save(matches);
 
+    }
+
+    public void compute(String word) {
+        matches.put(word, matches.getOrDefault(word, 0) + 1);
     }
 
     @Override
