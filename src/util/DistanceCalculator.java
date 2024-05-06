7ed7b8c4bcfa59a0c4603664460ea60caee2a467
Yuri Filgueira <yurimedeiros141@gmail.com>
2024-05-05 21:10:12 -0300

    Update: Name of variables

--- src/util/DistanceCalculator.java
+++ src/util/DistanceCalculator.java
@@ -1,28 +1,24 @@
 package util;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.locks.ReentrantLock;
 
 import static util.MatchingComputer.compute;
 
 public class DistanceCalculator implements Runnable{
 
     private List<String> lines;
-    private Map<String, Integer> matches;
-    private ReentrantLock mutex = new ReentrantLock();
+    private Map<String, Integer> counter;
+    
     public DistanceCalculator() {
     }
 
-    public DistanceCalculator(List<String> lines, Map<String, Integer> matches) {
+    public DistanceCalculator(List<String> lines, Map<String, Integer> counter) {
         this.lines = lines;
-        this.matches = matches;
+        this.counter = counter;
     }
 
     public void calculateDistance() {
-
-        List<String> items = new ArrayList<>(List.of("logitech", "keyboard", "mouse", "hyperx", "razer", "lenovo", "acer", "lg", "samsung", "laptop", "headset", "jbl"));
 
         System.out.println(Thread.currentThread().getName() + " -> started.");
 
@@ -31,13 +27,9 @@ public class DistanceCalculator implements Runnable{
             String rating = arrayRatingLine[2].replaceAll("\"", "").toLowerCase();
             String[] words = rating.split(" ");
 
-            for (String item : items) {
-
-                for (String word : words) {
-                    if (LevenshteinDistance.calculateDistance(word, item) == 0) {
-                        compute(word, matches);
-                        break;
-                    }
+            for (String word : words) {
+                if (LevenshteinDistance.calculateDistance(word, "mouse") == 0) {
+                    compute(word, counter);
                 }
             }
         }
--- src/util/MatchingComputer.java
+++ src/util/MatchingComputer.java
@@ -7,9 +7,9 @@ public class MatchingComputer {
 
     private static final ReentrantLock mutex = new ReentrantLock();
 
-    public static void compute(String word, Map<String, Integer> matches) {
+    public static void compute(String word, Map<String, Integer> counter) {
         mutex.lock();
-        matches.put(word, matches.getOrDefault(word, 0) + 1);
+        counter.put(word, counter.getOrDefault(word, 0) + 1);
         mutex.unlock();
     }
 }
