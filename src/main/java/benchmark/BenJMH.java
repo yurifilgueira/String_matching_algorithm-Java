1201bfe663c13b5f7d94d011634c7b43f7715f60
Yuri Filgueira <yurimedeiros141@gmail.com>
2024-06-16 20:31:33 -0300

    Feat: JMH

---
+++ src/main/java/benchmark/BenJMH.java
@@ -0,0 +1,57 @@
+package benchmark;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import readers.Reader;
+import util.DistanceCalculator;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Benchmark)
+public class BenJMH {
+
+    private Stack<List<String>> blocks;
+
+    @Setup(Level.Invocation)
+    public void setup() throws IOException {
+        Reader reader = new Reader();
+        blocks = reader.getBlocks();
+    }
+
+    @Benchmark
+    @BenchmarkMode({Mode.AverageTime, Mode.Throughput})
+    @Warmup(iterations = 5, batchSize = 1)
+    @Fork(value = 1)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public void benchCalculateDistance(Blackhole bh) throws InterruptedException {
+
+        List<Thread> threads = new ArrayList<>();
+
+        Map<String, Integer> counter = new HashMap<>();
+        for (int i = 0; i < 6; i++) {
+            Thread t = Thread.ofPlatform().name(String.valueOf(i)).unstarted(new DistanceCalculator(blocks.pop(), counter));
+            threads.add(t);
+            t.start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        bh.consume(counter.get("mouse"));
+    }
+
+    @Benchmark
+    @BenchmarkMode({Mode.AverageTime, Mode.Throughput})
+    @Warmup(iterations = 5)
+    @Fork(value = 1)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public void benchReadFile(Blackhole bh) throws IOException {
+
+        final List<String> lines = Reader.readFile();
+
+        bh.consume(lines);
+    }
+}
---
+++ src/main/java/benchmark/TestJMH.java
@@ -0,0 +1,11 @@
+package benchmark;
+
+import org.openjdk.jmh.Main;
+
+import java.io.IOException;
+
+public class TestJMH {
+    public static void main(String[] args) throws IOException {
+        Main.main(args);
+    }
+}
